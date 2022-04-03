8d1dbe8a386c670a993ad924500d7657ecd819c3
Hayreddin Tüzel <36648230+htuzel@users.noreply.github.com>
2022-04-03 15:25:07 +0200

    new NFT Contract

---
+++ constants.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x1E644C76e9de7927b61E0dbEBFE9A4BCA44408C9";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ test/cryptodevs.js
@@ -0,0 +1,39 @@
+const {
+    expect
+} = require("chai");
+const {
+    ethers
+} = require("hardhat");
+const {
+    set,
+    reset
+} = require("mockdate")
+
+describe("Crypto Devs", function () {
+    const contracts = {
+        whitelist: null,
+        cryptoDevs: null
+      }
+    beforeEach(async () => {
+        const [owner] = await ethers.getSigners();
+        const Whitelist = await ethers.getContractFactory("Whitelist");
+        const whitelist = await Whitelist.deploy("10");
+        await whitelist.deployed();
+        const CryptoDevs = await ethers.getContractFactory("CryptoDevs");
+        contracts.cryptoDevs = await CryptoDevs.deploy("10", whitelist.address);
+        await whitelist.addAddressToWhitelist();
+    })
+
+    it("Presale is started", async function () {
+        result = contracts.cryptoDevs.startPresale();
+        expect(await contracts.cryptoDevs.presaleStarted()).to.equal(true);
+    });
+
+    it("Presale is paused", async function () {
+
+        result = contracts.cryptoDevs.setPaused(true);
+        expect(await contracts.cryptoDevs._paused()).to.equal(true);
+        result = contracts.cryptoDevs.setPaused(false);
+        expect(await contracts.cryptoDevs._paused()).to.equal(false);
+    });
+});
